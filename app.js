5ee1b2a7707af27b74d0afe88ca82b35a73d8951
just_pulfer <djlydia.pd@gmail.com>
2023-10-18 19:46:59 -0600

    worked around the checkbox and bin icons with css

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const userInput = document.getElementById('userInput');
-const addBtn = document.getElementById('addBtn');
-const todoLists = document.getElementsByClassName('todoLists');
+const userInput = document.getElementById("userInput");
+const addBtn = document.getElementById("addBtn");
+const todoLists = document.getElementById("todoLists");
+
+addBtn.addEventListener("click", (e) => {
+    const list = document.createElement("li");
+    list.appendChild(userInput.value);
+    todoLists.appendChild(list);
+    console.log(list);
+})
+
