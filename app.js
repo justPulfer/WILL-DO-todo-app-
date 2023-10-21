4005e30b46fb3efbccaac062b67af6f711dc57fd
just_pulfer <djlydia.pd@gmail.com>
2023-10-20 22:11:17 -0600

    completed the section to add the task...next is adding the completed section and the checked button feature

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const userInput = document.getElementById("userInput");
+const inputTag = document.getElementById("userInput");
 const addBtn = document.getElementById("addBtn");
-const todoLists = document.getElementById("todoLists");
+const unorderedList = document.getElementById("todoLists");
 
 // Creating variables for the svg btns
 
@@ -11,31 +11,38 @@ const checkedSVG =
 	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"></path><path d="M10.996 12.556 9.7 11.285l-1.4 1.43 2.704 2.647 4.699-4.651-1.406-1.422z"></path></svg>';
 
 const trashSVG =
-	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>';
+	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #f01515;transform: ;msFilter:;"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>';
 
-function addWillDo() {
-	if (userInput.value) {
-		let list = document.createElement("li");
+// Function to create a new list item with a checkbox and trash button
+function createListItem(text) {
+	const listItem = document.createElement("li");
+	const listSpan = document.createElement("span");
+	const checkbox = document.createElement("input");
+	checkbox.type = "checkbox";
 
-		// Create btns
-		const uncheckedBtn = document.createElement("button");
-		uncheckedBtn.classList.add("listBtns");
-		uncheckedBtn.innerHTML = uncheckedSVG;
+	const label = document.createElement("label");
+	label.textContent = text;
 
-		const trashBtn = document.createElement("button");
-		trashBtn.classList.add("listBtns");
-		trashBtn.innerHTML = trashSVG;
+	const trashButton = document.createElement("button");
+	trashButton.innerHTML = trashSVG;
+	trashButton.classList.add("listItemBtn", "trashBtn");
 
-		// Group the li and checked btn
-		const listSpan = document.createElement("span");
-		listSpan.appendChild(uncheckedBtn);
+	listSpan.appendChild(checkbox);
+	listSpan.appendChild(label);
+	listItem.appendChild(listSpan);
+	listItem.appendChild(trashButton);
 
-		list.appendChild(listSpan);
-		list.appendChild(trashBtn);
-		todoLists.appendChild(list);
-	}
-
-	userInput.value = "";
+	return listItem;
 }
 
-addBtn.addEventListener("click", addWillDo);
+// Function to add list item to unordered list
+function addItem() {
+	if (inputTag.value) {
+		const listItem = createListItem(inputTag.value);
+		unorderedList.appendChild(listItem);
+	}
+	// clear the input field
+	inputTag.value = "";
+}
+
+addBtn.addEventListener("click", addItem);
