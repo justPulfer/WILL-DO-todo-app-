5e5dbf12cd29822b0c6976c5393a51510f9b0eef
Pulfer Dadson <just_pulfer@Trudys-iMac.gateway.sasktel.net>
2023-10-30 21:41:47 -0700

    Next step is to add and remove the completed text when checked

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const inputTag = document.getElementById("userInput");
 const addBtn = document.getElementById("addBtn");
-const unorderedList = document.getElementById("todoLists");
+const uncompletedList = document.getElementById("todoLists");
+const completedList = document.getElementById("completedList");
+const completedTag = document.getElementById("completedTag");
 
 // Creating variables for the svg btns
-
-//const uncheckedSVG =
-// '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm-1 16H5V5h14v14z"></path></svg>';
-
-// const checkedSVG =
-// 	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"></path><path d="M10.996 12.556 9.7 11.285l-1.4 1.43 2.704 2.647 4.699-4.651-1.406-1.422z"></path></svg>';
 
 const trashSVG =
 	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #f01515;transform: ;msFilter:;"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>';
@@ -37,13 +33,21 @@ function createListItem(text) {
 	listItem.appendChild(listSpan);
 	listItem.appendChild(trashButton);
 
-	// add listItem to completed section when checked
-	checkbox.addEventListener("change", () => {
+	// Event to add listItem to completed section when checked
+	checkbox.addEventListener("change", (evt) => {
 		if (checkbox.checked == true) {
-			const completedList = document.getElementById("completedList");
+			uncompletedList.removeChild(listItem);
 			completedList.appendChild(listItem);
+
+			// Function to display <p> when ul has li
+			if (completedList.childNodes.length > 0) {
+				completedTag.style.display = "block";
+			} else {
+				completedTag.style.display = "none";
+			}
 		} else {
-			// Move item back
+			completedList.removeChild(listItem);
+			uncompletedList.appendChild(listItem);
 		}
 	});
 
@@ -54,7 +58,7 @@ function createListItem(text) {
 function addItem() {
 	if (inputTag.value) {
 		const listItem = createListItem(inputTag.value);
-		unorderedList.appendChild(listItem);
+		uncompletedList.appendChild(listItem);
 	}
 	// clear the input field
 	inputTag.value = "";
