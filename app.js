ecdee557a68b3369ba05bdc4ec9551d1732af414
just_pulfer <djlydia.pd@gmail.com>
2023-11-03 18:14:07 -0600

    only display completed text when completed list has a list item

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const inputTag = document.getElementById("userInput");
 const addBtn = document.getElementById("addBtn");
 const unorderedList = document.getElementById("todoLists");
+const completedList = document.getElementById("completedList");
 
-// Creating variables for the svg btns
-
-//const uncheckedSVG =
-// '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M20 3H4a1 1 0 0 0-1 1v16a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1zm-1 16H5V5h14v14z"></path></svg>';
-
-// const checkedSVG =
-// 	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;"><path d="M7 5c-1.103 0-2 .897-2 2v10c0 1.103.897 2 2 2h10c1.103 0 2-.897 2-2V7c0-1.103-.897-2-2-2H7zm0 12V7h10l.002 10H7z"></path><path d="M10.996 12.556 9.7 11.285l-1.4 1.43 2.704 2.647 4.699-4.651-1.406-1.422z"></path></svg>';
-
+// Trash btn
 const trashSVG =
 	'<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" style="fill: #f01515;transform: ;msFilter:;"><path d="M5 20a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V8h2V6h-4V4a2 2 0 0 0-2-2H9a2 2 0 0 0-2 2v2H3v2h2zM9 4h6v2H9zM8 8h9v12H7V8z"></path><path d="M9 10h2v8H9zm4 0h2v8h-2z"></path></svg>';
 
@@ -40,10 +34,10 @@ function createListItem(text) {
 	// add listItem to completed section when checked
 	checkbox.addEventListener("change", () => {
 		if (checkbox.checked == true) {
-			const completedList = document.getElementById("completedList");
 			completedList.appendChild(listItem);
 		} else {
-			// Move item back
+			completedList.removeChild(listItem);
+			todoLists.appendChild(listItem);
 		}
 	});
 
@@ -61,3 +55,8 @@ function addItem() {
 }
 
 addBtn.addEventListener("click", addItem);
+inputTag.addEventListener("keydown", (e) => {
+	if (e.code === "Enter") {
+		addItem();
+	}
+});
